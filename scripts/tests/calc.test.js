226b26d14b7660219425ae28471a354c05271533
teman67 <>
2023-10-29 22:02:11 +0000

    initial testing

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,19 @@
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Additional function", () => {
+        test("should return 42 for 20 and 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+
+    });
+    describe("Subtraction function", () => {
+
+    });
+    describe("Multiply function", () => {
+
+    });
+    describe("Division function", () => {
+
+    });
+});
